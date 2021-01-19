19a00ec6b5d21ea6c8c791ab5717d1e5edb606a7
MayankSama <77658054+MayankSama@users.noreply.github.com>
2021-01-19 11:40:17 +0530

    Exchange_Rate

--- Exchange_Rate/src/runner/TestRunner.java
+++ Exchange_Rate/src/runner/TestRunner.java
@@ -1,16 +1,14 @@
 
 package runner;
+
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "functionalTests/End2End_Testing.feature",
-        glue = {"stepDefinitions/TestStep"},
-        monochrome = true,
-        strict = true)
+@CucumberOptions(features = "functionalTests/End2End_Testing.feature", glue = {
+		"stepDefinitions/TestStep" }, monochrome = true, strict = true)
 public class TestRunner {
-	
+
 }
--- Exchange_Rate/src/stepDefinitions/TestStep.java
+++ Exchange_Rate/src/stepDefinitions/TestStep.java
@@ -17,13 +17,14 @@ import io.restassured.specification.RequestSpecification;
 public class TestStep {
 
 	String latestValue = null;
+
 	@Given("^The API is available$")
 	public void LatestAPI() {
 
 		RestAssured.baseURI = "https://api.ratesapi.io/api/";
 		RequestSpecification request = RestAssured.given();
 		Response response = request.get("latest");
-			Assert.assertEquals(200, response.getStatusCode());
+		Assert.assertEquals(200, response.getStatusCode());
 
 	}
 
@@ -36,99 +37,101 @@ public class TestStep {
 		String jsonString = response.asString();
 		Assert.assertEquals(200, response.getStatusCode());
 	}
-	 @Then("^Get Success status in the response$")
-	 public void GetResponseLatestExchangeRate() {
-	 RestAssured.baseURI = "https://api.ratesapi.io/api/";
+
+	@Then("^Get Success status in the response$")
+	public void GetResponseLatestExchangeRate() {
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
 		RequestSpecification request = RestAssured.given();
 		Response response = request.get("latest");
 		Assert.assertEquals(200, response.getStatusCode());
-	 }
-	 
-	 @Then("^Get Exchange Rate in the response$")
-	 public void GetLatestExchangeRate() {
-	 RestAssured.baseURI = "https://api.ratesapi.io/api/";
+	}
+
+	@Then("^Get Exchange Rate in the response$")
+	public void GetLatestExchangeRate() {
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
 		RequestSpecification request = RestAssured.given();
 		Response response = request.get("latest");
 		String jsonString = response.asString();
-		 List<Map<String, String>> latestValue = JsonPath.from(jsonString).get("INR");
+		List<Map<String, String>> latestValue = JsonPath.from(jsonString).get("INR");
 		Assert.assertTrue(jsonString.contains("INR"));
-		 
-				 
-	 }
-	 @When("^Search with incorrect/incomplete API$")
-		public void SearchWithIncorrectAPI() {
 
-			RestAssured.baseURI = "https://api.ratesapi.io/ap";
-			RequestSpecification request = RestAssured.given();
-			Response response = request.get("latest");
-			String jsonString = response.asString();
-			Assert.assertTrue(jsonString.contains("Error"));
-	 }
-	 
-	 @Then("^Error message displayed in response$")
-		public void ErrorMessageInResponse() {
+	}
 
-			RestAssured.baseURI = "https://api.ratesapi.io/ap";
-			RequestSpecification request = RestAssured.given();
-			Response response = request.get("latest");
-			String jsonString = response.asString();
-			Assert.assertTrue(jsonString.contains("Error"));
-	 }
-	 
-	 @When("^Search Exchange rates for Specific Date$")
-		public void SearchExchangeRateForSpecificDate() {
+	@When("^Search with incorrect/incomplete API$")
+	public void SearchWithIncorrectAPI() {
 
-			RestAssured.baseURI = "https://api.ratesapi.io/api/";
-			RequestSpecification request = RestAssured.given();
-			Response response = request.get("2020-11-11");
-			String jsonString = response.asString();
+		RestAssured.baseURI = "https://api.ratesapi.io/ap";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("latest");
+		String jsonString = response.asString();
+		Assert.assertTrue(jsonString.contains("Error"));
+	}
+
+	@Then("^Error message displayed in response$")
+	public void ErrorMessageInResponse() {
+
+		RestAssured.baseURI = "https://api.ratesapi.io/ap";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("latest");
+		String jsonString = response.asString();
+		Assert.assertTrue(jsonString.contains("Error"));
+	}
+
+	@When("^Search Exchange rates for Specific Date$")
+	public void SearchExchangeRateForSpecificDate() {
+
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("2020-11-11");
+		String jsonString = response.asString();
+		Assert.assertEquals(200, response.getStatusCode());
+	}
+
+	@Then("^Get Success status in the response for specific date$")
+	public void GetResponseExchangeRateOfSpecificDate() {
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("2020-11-11");
+		Assert.assertEquals(200, response.getStatusCode());
+	}
+
+	@Then("^Get Exchange Rate of the Specific Date in response$")
+	public void GetExchangeRateOfSpecificDate() {
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("2021-11-11");
+		String jsonString = response.asString();
+		Assert.assertTrue(jsonString.contains("INR"));
+	}
+
+	@When("^Search Exchange rates for Future Date$")
+	public void SearchExchangeRateForFutureDate() {
+
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("2021-11-11");
+		String jsonString = response.asString();
+		Assert.assertEquals(200, response.getStatusCode());
+	}
+
+	@Then("^Get Success status in the response for Future Date$")
+	public void GetResponseExchangeRateOfFutureDate() {
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("2021-11-11");
+		Assert.assertEquals(200, response.getStatusCode());
+	}
+
+	@Then("^Get Latest Exchange Rate in response for Future Date$")
+	public void GetExchangeRateOfFutureDate() {
+		RestAssured.baseURI = "https://api.ratesapi.io/api/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("2021-11-11");
+		String jsonString = response.asString();
+		List<Map<String, String>> currentValue = JsonPath.from(jsonString).get("INR");
+		if (latestValue.equals(currentValue)) {
 			Assert.assertEquals(200, response.getStatusCode());
 		}
-		 @Then("^Get Success status in the response for specific date$")
-		 public void GetResponseExchangeRateOfSpecificDate() {
-		 RestAssured.baseURI = "https://api.ratesapi.io/api/";
-			RequestSpecification request = RestAssured.given();
-			Response response = request.get("2020-11-11");
-			Assert.assertEquals(200, response.getStatusCode());
-		 }
-		 
-		 @Then("^Get Exchange Rate of the Specific Date in response$")
-		 public void GetExchangeRateOfSpecificDate() {
-		 RestAssured.baseURI = "https://api.ratesapi.io/api/";
-			RequestSpecification request = RestAssured.given();
-			Response response = request.get("2021-11-11");
-			String jsonString = response.asString();
-			Assert.assertTrue(jsonString.contains("INR"));
-			}
-		 
-		 @When("^Search Exchange rates for Future Date$")
-			public void SearchExchangeRateForFutureDate() {
+	}
 
-				RestAssured.baseURI = "https://api.ratesapi.io/api/";
-				RequestSpecification request = RestAssured.given();
-				Response response = request.get("2021-11-11");
-				String jsonString = response.asString();
-				Assert.assertEquals(200, response.getStatusCode());
-			}
-			 @Then("^Get Success status in the response for Future Date$")
-			 public void GetResponseExchangeRateOfFutureDate() {
-			 RestAssured.baseURI = "https://api.ratesapi.io/api/";
-				RequestSpecification request = RestAssured.given();
-				Response response = request.get("2021-11-11");
-				Assert.assertEquals(200, response.getStatusCode());
-			 }
-			 
-			 @Then("^Get Latest Exchange Rate in response for Future Date$")
-			 public void GetExchangeRateOfFutureDate() {
-			 RestAssured.baseURI = "https://api.ratesapi.io/api/";
-				RequestSpecification request = RestAssured.given();
-				Response response = request.get("2021-11-11");
-				String jsonString = response.asString();
-				 List<Map<String, String>> currentValue = JsonPath.from(jsonString).get("INR");
-				 if (latestValue.equals(currentValue)) {
-					 Assert.assertEquals(200, response.getStatusCode());
-				 }
-				}
-
-			
 }
